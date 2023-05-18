2517f6202aa87ed268f283c2e296ae4c1338e1c6
Anton Kalashnikov <kalashnikov004@gmail.com>
2023-05-18 09:49:22 +0200

    added admin retrieving from db

--- DataAccessLibrary/Models/DBUser.cs
+++ DataAccessLibrary/Models/DBUser.cs
@@ -15,6 +15,7 @@ namespace DataAccessLibrary.Models
         private string name;
         private string hashedPassword;
         private int currentCredits;
+        private bool isAdmin = false;
 
         public DBUser(string name, string hashedPassword, int currentCredits)
         {
@@ -23,9 +24,9 @@ namespace DataAccessLibrary.Models
             CurrentCredits = currentCredits;
         }
 
-        public DBUser(int iD, string name, string hashedPassword, int currentCredits)
+        public DBUser(int ID, string name, string hashedPassword, int currentCredits)
         {
-            ID = iD;
+            this.ID = ID;
             Name = name;
             HashedPassword = hashedPassword;
             CurrentCredits = currentCredits;
@@ -38,11 +39,19 @@ namespace DataAccessLibrary.Models
             CurrentCredits = 0;
         }
 
+        public DBUser(int iD, string name, string hashedPassword, int currentCredits, bool isAdmin)
+        {
+            ID = iD;
+            Name = name;
+            HashedPassword = hashedPassword;
+            CurrentCredits = currentCredits;
+            IsAdmin = isAdmin;
+        }
+
         public int ID { get { return id; } set { this.id = value; } }
         public string Name { get => name; set => name = value; }
         public string HashedPassword { get => hashedPassword; set => hashedPassword = value; }
         public int CurrentCredits { get => currentCredits; set => currentCredits = value; }
-
-
+        public bool IsAdmin { get => isAdmin; set => isAdmin = value; }
     }
 }
