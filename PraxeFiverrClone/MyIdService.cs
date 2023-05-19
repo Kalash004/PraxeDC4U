ab71c7fb83e80aff7cd882e0a8189b66983b1a11
Maxwell Olsbro <max.olsbro@gmail.com>
2023-05-19 12:23:27 +0200

    global variable session Id added

---
+++ PraxeFiverrClone/MyIdService.cs
@@ -0,0 +1,7 @@
+﻿namespace PraxeFiverrClone
+{
+    public class MyIdService
+    {
+        public SessionService.SessionTemplate_Creater.SessionId id { get; set; } = new SessionService.SessionTemplate_Creater.SessionId("");
+    }
+}
