ec031c7b73838eea985008ad41513e81f8ba3144
Anton Kalashnikov <kalashnikov004@gmail.com>
2023-05-19 12:55:29 +0200

    A - Fixed small problem with sessionid reading, fixed tests, fixed updating

---
+++ DBMSTest/DBServiceReadTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary;
+using DataTemplateLibrary.Models;
+using SessionService.SessionTemplate_Creater;
+
+namespace Tests
+{
+    [TestClass]
+    public class DBServiceReadTest
+    {
+        [TestMethod]
+        public void ReadingServicesWithSessionID()
+        {
+            // Arrange
+            DBManager manager = new DBManager();
+            DBUser user = new DBUser("1455980865", "565440666");
+            var data = manager.LogUserInCreateSession(user);
+            SessionId sessionId = data.Result;
+            // Act
+            List<DBService?> services = manager.GetAllUserServices(sessionId);
+            DBService concrete_service = manager.GetServiceFromDB(sessionId, services.First().ID);
+            // Assert
+            Assert.IsNotNull(services);
+            Assert.IsTrue(services.Any());
+            Assert.AreEqual(concrete_service.ServiceName, services.First().ServiceName);
+            
+        } 
+    }
+}
