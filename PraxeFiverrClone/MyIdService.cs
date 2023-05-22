4df306b05e97b5a5faad4767478fc6b42f93ff54
Maxwell Olsbro <max.olsbro@gmail.com>
2023-05-22 13:56:07 +0200

    improved create page, added individual store pages

--- PraxeFiverrClone/MyIdService.cs
+++ PraxeFiverrClone/MyIdService.cs
@@ -1,7 +1,10 @@
-﻿namespace PraxeFiverrClone
+﻿using DataTemplateLibrary.Models;
+
+namespace PraxeFiverrClone
 {
     public class MyIdService
     {
         public SessionService.SessionTemplate_Creater.SessionId id { get; set; } = new SessionService.SessionTemplate_Creater.SessionId("");
+        public DBUser User { get; set; } = new DBUser("", "");
     }
 }
