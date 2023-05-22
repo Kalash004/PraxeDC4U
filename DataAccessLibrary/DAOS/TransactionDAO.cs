7fa2a8c36b7c53bdc4d3780be962b1efcff94a48
Anton Kalashnikov <kalashnikov004@gmail.com>
2023-05-22 13:49:01 +0200

    A - Working on Transactions

--- DataAccessLibrary/DAOS/TransactionDAO.cs
+++ DataAccessLibrary/DAOS/TransactionDAO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using DataTemplateLibrary.Interfaces;
@@ -13,13 +14,73 @@ namespace DataAccessLibrary.DAOS
     internal class TransactionDAO : AbstractDAO<DBTransaction>, IDAO<DBTransaction>
     {
         private static string table_n = "transactions";
-        private String C_CREATE = $"INSERT INTO {table_n} (user_id, ser_name, current_price, creation,`update`, isShown, short_description, long_description, link, isDeleted) VALUES (@user_id, @ser_name, @current_price, @creation, @update, @isShown, @short_description, @long_description, @link, @isDeleted)";
-        private String C_UPDATE = $"UPDATE {table_n} SET user_id = @user_id, ser_name = @ser_name, current_price = @current_price, creation = @creation, `update` = @update, isShown = @isShown, short_description = @short_description, long_description = @long_description, link = @link, isDeleted = @isDeleted WHERE id = @id";
-        private String C_READ_ALL = $"SELECT * FROM {table_n}";
-        private String C_READ_BY_ID = $"SELECT * FROM {table_n} WHERE id = @id";
-        private String C_DELETE = $"DELETE FROM {table_n} WHERE id = @id";
-        private String C_GET_BY_USER_ID = $"SELECT * FROM {table_n} WHERE user_id = @user_id";
-        private List<string> atributeList = new List<string>() { "id", "reciever_id", "sender_id", "send_date", "send_time", "cost", "service_id" };
+        // (data) values (@data)
+        private static String C_CREATE = $"INSERT INTO {table_n}";
+        // data = @data,
+        private static String C_UPDATE = $"UPDATE {table_n} SET WHERE id = @id";
+        private static String C_READ_ALL = $"SELECT * FROM {table_n}";
+        private static String C_READ_BY_ID = $"SELECT * FROM {table_n} WHERE id = @id";
+        private static String C_DELETE = $"DELETE FROM {table_n} WHERE id = @id";
+        private static String C_GET_BY_USER_ID = $"SELECT * FROM {table_n} WHERE user_id = @user_id";
+
+        private static List<string> atributeList = new List<string>() { "id", "reciever_id", "sender_id", "send_date", "send_time", "cost", "service_id" };
+
+        public TransactionDAO()
+        {
+            SetSQL(atributeList);
+        } 
+        private static void SetSQL(List<string> atributes)
+        {
+            SetSQLCreate(atributes);
+            SetSQLUpdate(atributes);
+        }
+
+        private static void SetSQLUpdate(List<string> atributes)
+        {
+            string last = atributes.Last();
+            foreach (var atribute in atributes)
+            {
+                if (atribute == last)
+                {
+                    C_UPDATE += $"{atribute} = @{atribute},";
+                }
+                else
+                {
+                    C_UPDATE += $"{atribute} = @{atribute},";
+                }
+            }
+        }
+
+        private static void SetSQLCreate(List<string> atributes)
+        {
+            C_CREATE += "(";
+            string last = atributes.Last();
+            foreach (var atribute in atributes)
+            {
+                if (atribute == last)
+                {
+                    C_CREATE += $"{atribute}";
+                }
+                else
+                {
+                    C_CREATE += $"{atribute},";
+                }
+            }
+            C_CREATE += ") values (";
+            foreach (var atribute in atributes)
+            {
+                if (atribute == last)
+                {
+                    C_CREATE += $"@{atribute}";
+                }
+                else
+                {
+                    C_CREATE += $"@{atribute},";
+                }
+            }
+            C_CREATE += ")";
+        }
+
         public int Create(DBTransaction element)
         {
             throw new NotImplementedException();
